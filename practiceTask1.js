ed1809248e045a5a38baf6fc50e7e201a0ff87de
Kashuckiy <nazar.rybchuk1999@gmail.com>
2019-04-18 01:40:37 +0300

    new file

--- practiceTask1.js
+++ practiceTask1.js
@@ -7,7 +7,6 @@ function task1() {
 	alert("Площа = " + s);
 	alert("Периметр = " + p);
 }
-//\\//\\//\\//\\//\\//\\//\\//\\//\\//\//\\
 function task2() {
 	var sum, dob, ser;
 	var a = prompt();
@@ -19,4 +18,30 @@ function task2() {
 	document.write(a + "+" + b + "+" + c + "=" + sum );
 	document.write(a + "*" + b + "*" + c + "=" + dob );
 	document.write("(" + a + "+" + b + "+" + c + ") / 3" + "=" + ser);
+}
+function task3(){
+	var sum, ser;
+	var a = prompt();
+	var b = prompt();
+	var c = prompt();
+	sum = +a + +b + +c;
+	ser = sum / 3;
+	document.write("(" + a + "+" + b + "+" + c + ") / 3" + "=" + ser.tofixed(3)).getElementById('window');
+}
+function task4(){
+	var num = prompt();
+	var prev, next;
+	prev = num--;
+	next = num ++;
+	document.write("The next number for the number" + num + "is" + next);
+	document.write("The previous number for the number" + num "is" + prev);
+}
+function task5(){
+	var a = prompt();
+	var b = prompt();
+	var temp = a;
+	a = b;
+	b = temp;
+	document.write("a = " + a);
+	document.write("b = " + b);
 }
